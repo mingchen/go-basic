c55af4117d0fb38fd4eb428b621b7969f5725b4f
Ming <mingchen@users.noreply.github.com>
2020-03-22 12:24:20 -0700

    Update env.go

--- env.go
+++ env.go
@@ -1,3 +1,12 @@
+// To set a key/value pair, use `os.Setenv`.
+//
+// To get a value for a key, use `os.Getenv`.
+// This will return an empty string if the key isn’t present in the environment.
+//
+// Use `os.Environ` to list all key/value pairs in the environment.
+// This returns a slice of strings in the form `KEY=value`. You can `strings.SplitN` them to get the key and value.
+//
+
 package main
 
 import (
